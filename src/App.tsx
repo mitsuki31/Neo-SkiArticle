3a884309e7d1b1315264f8b7b6c8b68dfae366c8
Ryuu Mitsuki <dhefam31@gmail.com>
2025-04-24 19:29:22 +0700

    chore(vercel): Integrate with Vercel speed insight

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import Home from "./pages/Home";
-import Header from "./pages/Header";
-import About from "./pages/About";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
+import Home from "@/pages/Home";
+import Header from "@/pages/Header";
+import About from "@/pages/About";
+import Footer from "@/pages/Footer";
 
 export default function App() {
   return (
@@ -8,7 +11,8 @@ export default function App() {
       <Header />
       <Home />
       <About />
-      {/* <Footer /> */}
+      <Footer />
+      <SpeedInsights />
     </>
   );
 }
