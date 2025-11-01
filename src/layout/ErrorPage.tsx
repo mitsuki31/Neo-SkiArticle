aac5359b7dd403d529299ad8daec7bb78d411589
Ryuu Mitsuki <dhefam31@gmail.com>
2025-11-02 00:30:05 +0700

    ref(layout): Improve the error page

--- src/layout/ErrorPage.tsx
+++ src/layout/ErrorPage.tsx
@@ -9,14 +9,16 @@ interface ErrorPageProps {
 
 function createErrorBody(errno: number, message: string, description: string) {
   return (
-    <div className={`text-center py-50 md:py-35 dark:bg-zinc-900 dark:text-white bg-white text-black}`}>
+    <div className="flex flex-col items-center justify-center h-svh text-center dark:bg-zinc-900 dark:text-white bg-white text-black">
       <h1 className="text-6xl md:text-8xl font-bold mb-5 font-inter">
         <span className="animate-blink">{errno}</span>
       </h1>
-      <p className="mt-2 text-lg text-center max-w-3xl mx-auto text-gray-600 dark:text-white/80 leading-8">
+
+      <p className="mt-2 text-lg max-w-3xl mx-auto text-gray-600 dark:text-white/80 leading-8">
         {message}
       </p>
-      <p className="mt-10 text-md text-center max-w-md md:max-w-3xl mx-auto text-gray-800 dark:text-white/60 leading-8 opacity-70 italic">
+
+      <p className="mt-6 text-md max-w-md md:max-w-3xl mx-auto text-gray-800 dark:text-white/60 leading-8 opacity-70 italic">
         {description}
       </p>
     </div>
